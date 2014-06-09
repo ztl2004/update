37b699418e8412902a0f298916c2ab47840daaad
Chen Liang <chliang2030598@gmail.com>
2014-06-09 13:10:45 +0800

    Add more property

--- models.go
+++ models.go
@@ -1,33 +1,22 @@
 package main
 
 import (
+  "github.com/arkors/update/model"
   _ "github.com/go-sql-driver/mysql"
   "github.com/go-xorm/xorm"
   "log"
 )
 
-type Version struct {
-  Id         int64
-  App        int64
-  Version    string
-  Name       string
-  Update     string
-  Changed    string
-  Url        string
-  Client     string
-  Compatible string
-}
-
 var x *xorm.Engine
 
 func init() {
   var err error
-  x, err = xorm.NewEngine("mysql", "root:root@/update?charset=utf8")
+  x, err = xorm.NewEngine("mysql", "arkors:arkors@/arkors_update?charset=utf8")
   if err != nil {
     log.Fatalf("Fail to create engine: %v\n", err)
   }
 
-  if err = x.Sync(new(Version)); err != nil {
+  if err = x.Sync(new(model.Version)); err != nil {
     log.Fatalf("Fail to sync database: %v\n", err)
   }
 }
