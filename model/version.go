0b3cd916b33ace8927ef2d3bfcf243d2915bcd1a
Chen Liang <chliang2030598@gmail.com>
2014-06-09 13:29:39 +0800

    Add more property

--- model/version.go
+++ model/version.go
@@ -2,16 +2,17 @@ package model
 
 import (
   _ "github.com/go-sql-driver/mysql"
+  "time"
 )
 
 type Version struct {
   Id         int64
   App        int64
-  Version    string
-  Name       string
-  Update     string
-  Changed    string
-  Url        string
-  Client     string
-  Compatible string
+  Version    string    `xorm:"text"`
+  Name       string    `xorm:"text"`
+  Updated    time.Time `xorm:"updated"`
+  Changed    time.Time `xorm:"changed"`
+  Url        string    `xorm:"text"`
+  Client     string    `xorm:"text"`
+  Compatible string    `xorm:"text"`
 }
